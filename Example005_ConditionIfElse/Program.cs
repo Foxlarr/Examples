d95b24ebdc4c26a2868ffbbe7245eb48661f7a35
Foxxlarr <talismafeed@yandex.ru>
2022-09-18 17:26:04 +0300

    Hi spesial user

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Input user name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "fox")
+{
+Console.WriteLine("Hey! It's you again!");
+}
+else
+{
+Console.Write("Hi, ");
+Console.WriteLine(username);
+}
